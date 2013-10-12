1c62d9e1a5398a3fc20a83ca8c39e909a02f3682
JasonJasonJason <jasonharris1991@gmail.com>
2013-10-11 22:27:10 -0500

    Added 'stepped' overlay
    The overlay will follow a predefined pattern when clicked!

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var menu_width = 200;
 var color_value = 0;
 var video_step = 0;
+var overlaySteps=new Array([10,10,100,100],[200,200,50,100],[100,300,100,50]);
 
 window.onload=function(){
 
@@ -48,13 +49,16 @@ function drawMenu(context) {
 
 function highlightNextArea(){
 
+
 	var overlay = document.getElementById("overlayLayer");
     if (overlay.getContext) {
 
-    	area_x = 100;
-    	area_y = 100;
-    	area_width = 100;
-    	area_height = 100;
+    	nextArea = overlaySteps[video_step % overlaySteps.length];
+
+    	area_x = nextArea[0];
+    	area_y = nextArea[1];
+    	area_width  = nextArea[2];
+    	area_height = nextArea[3];
 
     	var context = overlay.getContext("2d");
 
@@ -63,10 +67,29 @@ function highlightNextArea(){
     	context.fillRect(menu_width, 0, overlay.width - menu_width, overlay.height);
 
     	//Cut out the focus square
+    	previousComposite = context.globalCompositeOperation;
     	context.globalCompositeOperation = "destination-out";
 		context.fillStyle = "rgb(255,255,255)";
 		context.fillRect(menu_width + area_x, area_y, area_width, area_height);
-		
+		context.globalCompositeOperation = previousComposite;
+    }
+
+}
+
+
+function clearOverlay(){
+
+	var overlay = document.getElementById("overlayLayer");
+    if (overlay.getContext) {
+
+    	var context = overlay.getContext("2d");
+
+    	//Cut out the focus square
+    	previousComposite = context.globalCompositeOperation;
+    	context.globalCompositeOperation = "destination-out";
+		context.fillStyle = "rgb(255,255,255)";
+		context.fillRect(menu_width, 0, overlay.width-menu_width, overlay.height);
+		context.globalCompositeOperation = previousComposite;
     }
 
 }
@@ -79,6 +102,7 @@ function highlightNextArea(){
 
 function onCanvasClicked(e){
 
+	clearOverlay();
 	video_step += 1;
 	highlightNextArea();
 }
